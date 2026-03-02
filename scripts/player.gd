3fc8f2832c0258af786ae377b89a3be0ff1d116e
Doko_Bhai <samarhardab@gmail.com>
2026-03-02 15:49:54 -0800

    Grapple

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,24 +6,25 @@ const JUMP_VELOCITY = -400.0
 const ACCELERATION = 0.1
 const DECELERATION = 0.1
 
+@onready var gc = $GrappleController
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("move_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("move_up") && (is_on_floor() || gc.launched):
+		velocity.y += JUMP_VELOCITY
+		gc.retract()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = lerp(velocity.x, SPEED * direction, ACCELERATION)
 	else:
 		velocity.x = lerp(velocity.x, SPEED * direction, DECELERATION)
 		
-	if Input.is_action_pressed("move_down") and is_on_floor():
+	if Input.is_action_pressed("move_down") && is_on_floor():
 		$Sprite2D.scale.y = 0.05
 	else:
 		$Sprite2D.scale.y = 0.1
