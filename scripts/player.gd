53b7e5f75ee3c2dbddaf85f7480fb9ff5fbcda3b
Doko_Bhai <samarhardab@gmail.com>
2026-03-02 16:05:58 -0800

    Test

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,7 +13,6 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("move_up") && (is_on_floor() || gc.launched):
 		velocity.y += JUMP_VELOCITY
 		gc.retract()
